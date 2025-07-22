30d486794e6ca8427a5f7d4c6bc1640d516b7c6f
HyperPaint <dioman172@ya.ru>
2025-07-22 23:51:59 +1000

    Кавычки

--- docker-build.sh
+++ docker-build.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-repository=frog-is-mine
-name=minecraft.rcon
-version=1.0.0
-mcrcon_version=0.7.2
+repository="frog-is-mine"
+name="minecraft.rcon"
+version="1.0.0"
+mcrcon_version="0.7.2"
 
-docker build -t $repository/$name:$version-$mcrcon_version .
+docker build -t "$repository/$name:$version-$mcrcon_version" .
